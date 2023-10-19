83916175d8882a859b06a75fb4d40354034d8e04
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-19 15:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+        // Function to detect the user's browser and version
+        function detectBrowser() {
+            var browserInfo = "Browser: " + navigator.appName + "<br>Version: " + navigator.appVersion;
+
+            // Display the information in the browser-info div
+            document.getElementById("browser-info").innerHTML = browserInfo;
+        }
+
+        // Call the detectBrowser function when the page loads
+        window.onload = detectBrowser;
+    </script>
