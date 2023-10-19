af7cbf5811356419096d8dc31050c74d955f42c2
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-19 15:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
-        // Function to detect the user's browser and version
-        function detectBrowser() {
-            var browserInfo = "Browser: " + navigator.appName + "<br>Version: " + navigator.appVersion;
+function displayBrowserInfo() {
+            var browserName = navigator.appName;
+            var browserVersion = navigator.appVersion;
+            
+            // Create a message with the browser name and version
+            var message = "You are using " + browserName + " version " + browserVersion;
 
             // Display the information in the browser-info div
-            document.getElementById("browser-info").innerHTML = browserInfo;
+            var browserInfoDiv = document.getElementById("browser-info");
+            browserInfoDiv.innerHTML = message;
         }
 
-        // Call the detectBrowser function when the page loads
-        window.onload = detectBrowser;
-    </script>
+        // Call the displayBrowserInfo function when the page loads
+        window.onload = displayBrowserInfo;
